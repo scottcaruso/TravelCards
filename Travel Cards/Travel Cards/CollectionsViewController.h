6b5462daade9e9a1b3eb9d3b3485b5e27c7d9118
Scott Caruso <scottcaruso@fullsail.edu>
2014-06-23 23:36:55 -0500

    Very basic collection view working.

---
+++ Travel Cards/Travel Cards/CollectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionsViewController.h
+//  Travel Cards
+//
+//  Created by Scott Caruso on 6/23/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionsViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource>
+
+@end
---
+++ Travel Cards/Travel Cards/CollectionsViewController.m
@@ -0,0 +1,70 @@
+//
+//  CollectionsViewController.m
+//  Travel Cards
+//
+//  Created by Scott Caruso on 6/23/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import "CollectionsViewController.h"
+
+@interface CollectionsViewController ()
+
+@end
+
+@implementation CollectionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    if (cell == nil)
+    {
+        cell.backgroundColor = [UIColor whiteColor];
+    }
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
