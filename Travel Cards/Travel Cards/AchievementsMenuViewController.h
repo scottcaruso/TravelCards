3a7857aab11b8bacc4796239610316b57bc70942
Scott Caruso <scottcaruso@fullsail.edu>
2014-07-09 23:08:33 -0500

    Added new Leaderboards screen.

---
+++ Travel Cards/Travel Cards/AchievementsMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  AchievementsMenuViewController.h
+//  Travel Cards
+//
+//  Created by Scott Caruso on 7/9/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AchievementsMenuViewController : UIViewController <UITableViewDelegate>
+
+@end
