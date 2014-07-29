88ed042a40e263b96891b3e970cf6f9d11826ffa
Scott Caruso <scottcaruso@fullsail.edu>
2014-07-28 22:12:32 -0500

    Created and hooked up outlets for the many buttons.

---
+++ Travel Cards/Travel Cards/PartnerAdminViewController.h
@@ -0,0 +1,29 @@
+//
+//  PartnerAdminViewController.h
+//  Travel Cards
+//
+//  Created by Scott Caruso on 7/28/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartnerAdminViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
+{
+    IBOutlet UILabel *availability;
+    IBOutlet UILabel *onLabel;
+    IBOutlet UILabel *offLabel;
+    IBOutlet UILabel *noLabel;
+    IBOutlet UILabel *yesLabel;
+    IBOutlet UILabel *limitedDeal;
+    IBOutlet UILabel *maxViews;
+    IBOutlet UITextView *textField;
+    IBOutlet UIButton *cityName;
+    IBOutlet UIButton *landmarkName;
+    IBOutlet UISwitch *onOff;
+    IBOutlet UISwitch *yesNo;
+    IBOutlet UITextField *numberOfDeal;
+    IBOutlet UIPickerView *pickerView;
+}
+
+@end
